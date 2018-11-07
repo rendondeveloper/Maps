7027d8db935806270a4280e757796e959e3f4829
O-LRENDON <12345678>
2018-11-07 16:21:43 -0600

    Se agrega marcadores de posiciones fijas. Se agregan marcado de poligonos.

--- app/src/main/java/com/example/rendondev/maps/MapsActivity.java
+++ app/src/main/java/com/example/rendondev/maps/MapsActivity.java
@@ -1,18 +1,31 @@
 package com.example.rendondev.maps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GPSTracker tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +34,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        init();
         mapFragment.getMapAsync(this);
     }
 
+    private void init()
+    {
+        tracker = new GPSTracker(this);
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng locationCurrent;
+
+        mMap.setMyLocationEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            mMap.getUiSettings().setScrollGesturesEnabled(true);
+        }
+
+        if(tracker.canGetLocation())
+        {
+            locationCurrent = new LatLng(tracker.getLatitude(), tracker.getLongitude());
+        }
+        else
+        {
+            tracker.showSettingsAlert();
+            locationCurrent = new LatLng(-34, 151);
+        }
+
+        final CameraPosition MEX = new CameraPosition.Builder().target(locationCurrent).zoom(15.0f).build();
+        final CameraUpdate update = CameraUpdateFactory.newCameraPosition(MEX);
+        mMap.moveCamera(update);
+
+        final List<LatLng> list = geList();
+        for (LatLng item:
+        list) {
+           mMap.addMarker(new MarkerOptions().position(item).title("Concesionaria").snippet("Bajaj SA de CV").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        }
+
+        poligono() ;
+        //mMap.addMarker(new MarkerOptions().position(locationCurrent).title("Ubicacion Actual").icon());
+
+    }
+
+
+    private List<LatLng> geList()
+    {
+        final List<LatLng> list  = new ArrayList<>();
+
+        list.add(new LatLng(19.3935054,-99.1897021));
+        list.add(new LatLng(19.3854904,-99.1865695));
+        list.add(new LatLng(19.4063779,-99.1641248));
+        list.add(new LatLng(19.4132994,-99.1647256));
+        list.add(new LatLng(19.4279913,-99.1794456));
+        list.add(new LatLng(19.4316645,-99.1527834));
+        list.add(new LatLng(19.415732, -99.1476833));
+        list.add(new LatLng(19.4068344,-99.1320259));
+        list.add(new LatLng(19.4183045,-99.1294998));
+        return list;
+    }
+
+    public void poligono() {
+        final PolygonOptions opcionesPoligono = new PolygonOptions()
+                .add(new LatLng(19.3935054,-99.1897021))
+                .add(new LatLng(19.3854904,-99.1865695))
+                .add(new LatLng(19.4063779,-99.1641248))
+                .add(new LatLng(19.4132994,-99.1647256))
+                .add(new LatLng(19.4279913,-99.1794456));
+        Polygon poligono = mMap.addPolygon(opcionesPoligono);
+        poligono.setStrokeColor(Color.RED);
     }
 }
